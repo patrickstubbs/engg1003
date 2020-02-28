f6a71d1a91b6e65405975a736112e4fa60df6ebb
Patrick Stubbs <c3322878@uon.edu.au>
2020-02-28 15:41:05 +1100

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float x;
+    float y;
+    printf("Enter an number: ");
+    scanf("%f", &x); // Note change of %d to %f
+    // Complete the y = line below
+    y = (x+2)/(x-1);
+    printf("y: %f\n", y);
+    return 0;
+}
