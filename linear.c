23327c832a2d85f40ad2ec4af9994d8844b8c286
patrickstubbs <61573220+patrickstubbs@users.noreply.github.com>
2020-03-06 16:49:36 +1100

    Add files via upload

---
+++ linear.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    float x1, y1, x2, y2, y;
+    printf("Enter point1: ");
+    scanf("%f %f", &x1, &y1);
+    printf("Enter point 2: ");
+    scanf("%f %f")
+}
